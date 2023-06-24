17340287e6e3d51c9adf4b7a6360e0c6dba3636c
Ibrahim Oluwapeluwa <54927381+iPeluwa@users.noreply.github.com>
2023-06-25 00:01:18 +0100

    Fix2

--- rqm/settings.py
+++ rqm/settings.py
@@ -36,7 +36,7 @@ sentry_sdk.init(
 SECRET_KEY = 'django-insecure-xt5#bz(g+f1k1ttl-0@s80%gx7y+#s44$ka&b%g6)(v94y&31+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'userauth.User'
