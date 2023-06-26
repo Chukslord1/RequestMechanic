0759ebc312abcec294fdd0448370681611073a6c
Ibrahim Oluwapeluwa <54927381+iPeluwa@users.noreply.github.com>
2023-06-26 22:32:09 +0100

    Fix

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
 from userauth import views
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
+
 )
 
 
@@ -37,4 +39,4 @@ urlpatterns = [
 
 
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
